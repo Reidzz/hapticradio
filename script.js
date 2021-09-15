dd46498894d2d4488a2bc5a35a7d2bdb02c56bf1
Reidzz <danielreid2007@hotmail.co.uk>
2021-09-15 10:32:54 +0100

    Updated js script and created second mixcloud container in html

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-const pastShowToggler = document.querySelector(".past-shows");
+const ehfmShowToggler = document.querySelector(".ehfm-shows");
+const hapticSelectsToggler = document.querySelector(".haptic-selects");
 const mixCloudContainer = document.querySelector(".toggler");
+const hapticContainer = document.querySelector(".haptic");
+const exitIcons = document.querySelectorAll(".exit");
 
-pastShowToggler.addEventListener("click", function() {
-    mixCloudContainer.classList.toggle("open");
+ehfmShowToggler.addEventListener("click", function() {
+    mixCloudContainer.classList.add("open");
+    hapticContainer.classList.remove("open");
 })
+
+hapticSelectsToggler.addEventListener("click", function() {
+    mixCloudContainer.classList.remove("open");
+    hapticContainer.classList.add("open");
+})
+
+exitIcons.forEach(item => {
+    item.addEventListener("click", function() {
+        mixCloudContainer.classList.remove("open");
+        hapticContainer.classList.remove("open");
+    })
+})
+
+/*const navList = document.querySelectorAll(".nav-list-item");
+
+navList.forEach(item => {
+    item.addEventListener("click", function() {
+        mixCloudContainer.classList.toggle("open");
+        hapticContainer.classList.toggle("open");
+    })
+})*/
